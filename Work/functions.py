6278814660b7e7cb4827c4041a6d8c309046442d
Chris Uche <38696564+Chris-uche@users.noreply.github.com>
2021-05-22 22:20:40 +0100

    Function tutorial with  simple examples

---
+++ Work/functions.py
@@ -0,0 +1,33 @@
+def addthree(n):
+    return n + 2
+
+Newnumber = addthree(3)
+print(Newnumber)
+
+def subTractThree(m):
+    return m - 5
+
+print(subTractThree(10))
+
+
+
+def printString(string):
+    print(string)
+
+printString("Hello Chris")
+
+def tryTwoValue(num,param,vals):
+    return num + param + vals
+
+print(tryTwoValue(4,2,4))
+
+
+def register(firstname,lastname):
+     print(firstname,lastname)
+
+register("John","Chris")
+
+def massFunction(mass,force):
+    a = mass * force
+    return a
+print(massFunction(2,4))
