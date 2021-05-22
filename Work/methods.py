575e284f567e1fa65de5c8cb5a721ad3ee650e13
Chris Uche <38696564+Chris-uche@users.noreply.github.com>
2021-05-22 22:21:54 +0100

    Examples of methods and working exampls

---
+++ Work/methods.py
@@ -0,0 +1,17 @@
+string = "Hello, world!"
+#find
+print(string.find('o'))
+
+words = "Hello, wooorld"
+#count
+print(words.count("o"))
+
+
+stringify = input("Please make a statement here: ")
+if stringify.count("i") > 0:
+    print("This is not good")
+else:
+    print("good")
+
+chrisName = "Christogonus"
+print(chrisName.find("t"))
