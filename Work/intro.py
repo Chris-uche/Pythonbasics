7e1571ef3f3503962e7e018972abef6e12d4b839
Chris Uche <38696564+Chris-uche@users.noreply.github.com>
2021-05-22 21:54:13 +0100

    Python intro with examples

---
+++ Work/intro.py
@@ -0,0 +1,15 @@
+num1 = 1
+num2= 3
+numSum = num1 < num2
+print(numSum)
+
+age = input("Hello put your age: ")
+
+
+if int(age) >= 19:
+   print("Hello you are age is: 19 ")
+
+else:
+    print("You are younger please")
+
+    
