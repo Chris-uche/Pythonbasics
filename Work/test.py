e20845d5427e8dccd05c9c5d8f4338dbd8caf3d0
Chris Uche <38696564+Chris-uche@users.noreply.github.com>
2021-05-22 22:25:30 +0100

    conditional statements in python

---
+++ Work/test.py
@@ -0,0 +1,20 @@
+height = input("Put the height value here: ")
+
+if(int(height) >= 12):
+    print("You can ride Please")
+elif(int(height) <=11):
+    print("You cannot ride because your is below")
+else:
+    print("You can ride")
+
+
+age = input("Tell me your age before i can show you contents: ")
+
+if int(age) > 40:
+    print("Go and marry this man")
+elif int(age) < 20:
+    print("relax and make money")
+else:
+    print("Chill and enjoy")
+    
+    
